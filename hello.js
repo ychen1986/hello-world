73d1792fa51768aea4fcc6dabd88e4047747be02
ychen1986 <yin@ht.sfc.keio.ac.jp>
2018-12-25 14:05:48 +0900

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,4 +1,20 @@
-function click_callback(e) {
-    alert(e.latLng.toString());
-  }
-map.addListener('click', click_callback);
+function initMap() {
+  // 地図の中心を決める
+  var latlng = new google.maps.LatLng(35.388276, 139.427348);
+ 
+  // 地図を表示するためのオプションを設定する
+  var opt = {
+    zoom: 15,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+ 
+  // 地図を表示する。
+  var map = new google.maps.Map(document.getElementById('map'), opt);
+ 
+  // マーカーの追加
+  var marker = new google.maps.Marker({
+                     position: latlng,
+                     map: map
+        });
+}
